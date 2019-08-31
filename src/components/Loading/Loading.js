d8e5cc3bb6f94160f162e75152d2aa794819e38d
shuchao <645904877@qq.com>
2019-08-31 22:06:29 +0800

    loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Loading extends Component {
     render() {
         return (
-            <div>page loading...</div>
+            <div></div>
         )
     }
 }
