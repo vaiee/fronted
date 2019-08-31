044d9db7b1c8ebb6bb048c5717c9486883aadff8
shuchao <645904877@qq.com>
2019-08-31 22:24:09 +0800

    ...

--- src/pages/Help/freshman.js
+++ src/pages/Help/freshman.js
@@ -40,7 +40,7 @@ export default class Freshman extends Component {
                 <li>
                     <p className="title">3、如何重置密码？</p>
                     <p className="content_row">
-                    请登录www.vaiee.com点击「登录」，如何点击‘忘记密码？’，按照提示填写相应的资料进行密码重置。您还可以通过V速智能加速器客户端底部的「忘记密码」进行密码重置。
+                    官网登录成功后点击个人头像下方「忘记密码」，按照提示填写相应的资料进行密码重置。您还可以通过V速智能加速器客户端底部的「忘记密码」进行密码重置。
                     </p>
                 </li>
               </ul>
